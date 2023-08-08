5a08427c58016399a7c83b261f933fa969199020
Regen1337 <125764136+Regen1337@users.noreply.github.com>
2023-08-08 19:41:47 -0400

    .

--- turtles/stripper/load.lua
+++ turtles/stripper/load.lua
@@ -1,16 +1,10 @@
 local helper = http.get("https://raw.githubusercontent.com/Regen1337/misclua/main/turtles/helper/helper.lua").readAll()
 local miner = http.get("https://raw.githubusercontent.com/Regen1337/misclua/main/turtles/stripper/stripminer.lua").readAll()
 
-local helper_file = fs.open("turtle_helper.lua", "w")
-helper_file.write(helper)
-helper_file.close()
-
 local stripminer_file = fs.open("stripminer.lua", "w")
-stripminer_file.write(miner)
+stripminer_file.write(helper .. miner)
 stripminer_file.close()
 
-shell.run("turtle_helper.lua")
 shell.run("stripminer.lua")
 
-print("Loaded turtle_helper.lua")
 print("Loaded stripminer.lua\n")
