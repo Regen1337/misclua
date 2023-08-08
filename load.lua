eebd25cc361ce71cfea7f0df120eb2667fbc90d2
Regen1337 <regennukes@yahoo.com>
2023-08-08 12:50:54 -0400

    update

--- load.lua
+++ load.lua
@@ -1,8 +1,9 @@
-local url = "https://example.com/mycode.lua"
-local response = http.get(url)
-local content = response.readAll()
-response.close()
+local content = http.get("https://raw.githubusercontent.com/Regen1337/misclua/main/stripminer.lua").readAll()
 
-local file = fs.open("mycode.lua", "w")
+local file = fs.open("stripminer.lua", "w")
 file.write(content)
 file.close()
+
+print("Loaded stripminer.lua\n")
+
+shell.run("stripminer.lua")
